ae017410396fb392efe793f288241f538f724077
mihai10001 <rvivits@gmail.com>
2019-10-16 12:26:16 +0300

    finished filters

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, Blueprint, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from pillow import apply_blur
+from pillow import apply_blur, apply_sharpen, apply_edge_enhance, apply_smooth
 
 UPLOAD_FOLDER = os.getcwd() + '/static'
 ALLOWED_EXTENSIONS = set(['png', 'jpeg', 'jpg'])
@@ -12,6 +12,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 bp = Blueprint('photo_editor', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 # So preview refreshes with any new change
 @app.after_request
 def add_header(response):
@@ -20,11 +25,6 @@ def add_header(response):
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
     return response
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -65,13 +65,18 @@ def uploaded():
 
     if request.method == 'POST':
         blur_button = request.form.get('blur_button')
+        sharpen_button = request.form.get('sharpen_button')
+        edge_button = request.form.get('edge_button')
+        smooth_button = request.form.get('smooth_button')
 
         if blur_button:
             apply_blur(os.path.join(UPLOAD_FOLDER, INPUT_FILENAME), blur_button)
-
-            return redirect(url_for('photo_editor.uploaded'))
-        else:
-            return render_template('home.html')
+        elif sharpen_button:
+            apply_sharpen(os.path.join(UPLOAD_FOLDER, INPUT_FILENAME), sharpen_button)
+        elif edge_button:
+            apply_edge_enhance(os.path.join(UPLOAD_FOLDER, INPUT_FILENAME), edge_button)
+        elif smooth_button:
+            apply_smooth(os.path.join(UPLOAD_FOLDER, INPUT_FILENAME), smooth_button)
 
     if INPUT_FILENAME:
         return render_template('home.html', filename=INPUT_FILENAME)
