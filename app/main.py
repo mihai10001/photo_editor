bc793fe9945a55350952496f863b3ab77b9a3228
mihai10001 <rvivits@gmail.com>
2019-10-29 14:45:13 +0200

    changed port

--- app/main.py
+++ app/main.py
@@ -129,4 +129,4 @@ def uploaded():
 app.register_blueprint(bp, url_prefix='/photo_editor')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT'))
